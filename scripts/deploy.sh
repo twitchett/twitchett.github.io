bac961aaea5fdba7fe98b74c18f0fa58036ac040
Tabitha Goodall <tg.twitchett@gmail.com>
2017-04-15 21:40:31 +0200

    latest changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,15 +4,19 @@
 # it's only awkward because github requires that all source files are in root.
 # see: https://help.github.com/articles/generic-jekyll-build-failures/#source-setting
 
-# stash the changes we don't want to commit
+# stash untracked
 git stash save -u "stashing local modifications in preparation for commit to master" 
-# copy all contents of /src to root
+
+git checkout master
+# remove all git tracked files
+git ls-files -z | xargs -0 rm -f
+# remove all git tracked dirs
+git ls-tree --name-only -d -r -z HEAD | sort -rz | xargs -0 rmdir 
+# now we have a clean branch
+# copy across everything from source branch
+git checkout source -- .
+# copy the contents of /src to root
 cp -a src/. .
-# these are the changes we want to commit. stash them.
-git stash save -u "changes for master"
-# now checkout master and add changes
-git checkout master && git stash pop
 # add, commit, push
 git add . && git commit -m "latest changes" && git push origin master
-git checkout source # check the original branch
-git stash pop
+git checkout source git stash pop
