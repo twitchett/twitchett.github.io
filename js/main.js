aa09884e211a63a39f36985a43e8f62b6216750f
Tabitha Goodall <tg.twitchett@gmail.com>
2017-04-22 00:58:59 +0200

    static site

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
----
-layout: null
----
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
@@ -18,7 +15,7 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
-  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
+  if (window.location.pathname !== '/' && window.location.pathname !== '/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
