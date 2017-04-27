da91825ca832f089950baf895fe0e3e29aa5abb5
Tabitha Goodall <tg.twitchett@gmail.com>
2017-04-27 23:57:27 +0200

    minify production js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './react/app.js',
@@ -17,5 +18,10 @@ module.exports = {
                 presets: ['react', 'es2015']
             }
         }]
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: process.env.NODE_ENV === 'production'
+        }),
+    ]
 }
